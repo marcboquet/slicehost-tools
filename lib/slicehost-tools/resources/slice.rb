7c1fa1a1c1e8f4bd5f3715add7ee183eedff9c61
Cameron <cameroncox@gmail.com>
2008-07-02 04:52:55 -0400

    slicehost-slice lives supposedly

--- lib/slicehost-tools/resources/slice.rb
+++ lib/slicehost-tools/resources/slice.rb
@@ -1,10 +1,20 @@
 module Resources
-  class Slice < Base
+  class Slice < Base 
+    class << self
+      def find_by_name(name)                           
+        slice_id = find(:all).reject! { |r| r.name != name }.map { |slice| slice.id }
+        find(slice_id)
+      end
+    end
   end
   
   class Flavor < Base
+    def to_s; self.name; 
+    end
   end
   
   class Image < Base
+    def to_s; self.name; 
+    end
   end
 end
--- lib/slicehost-tools/tools/slice.rb
+++ lib/slicehost-tools/tools/slice.rb
@@ -2,7 +2,83 @@ require :"slicehost-tools" / :resources / :slice
 module Tools
   class Slice < Default
     
+    desc "add [SLICE NAME]", "add a new slice"
+    method_options :force => :boolean
+    def add(slice_name, opts)               
+      images  = Resources::Image.find(:all)
+      flavors = Resources::Flavor.find(:all)
+            
+      puts "Available Images: "      
+      image_id = select_image_from(images)   
+      
+      puts "Available Flavors: "
+      flavor_id = select_flavor_from(flavors)
+      
+      @add = false
+      # confirm  you want to do this, it does cost money
+      unless opts[:force]     
+        print "Are you sure you wanna do this? [y/N]: "
+        case STDIN.gets.chomp
+        when /y/i
+          @add = true
+        end
+      end
+
+      if @add
+        slice = Resources::Slice.new(:name => slice_name, :flavor_id => flavor_id, :image_id => image_id)
+        slice.save
+      end
+      
+    end
+    
+    desc "list", "list slices"
+    def list
+      Resources::Slice.find(:all).each do |slice|
+        puts "+ #{slice.name} (#{slice.ip_address})"
+      end
+    end
+
+    desc "hard_reboot [SLICE]", "perform a hard reboot"
+    def hard_reboot(slice_name = nil)
+      abort "please provide a slice name" if slice_name.nil?
+      
+      puts "(hard) rebooting #{slice_name}"                      
+      reboot(:hard_reboot, slice_name)
+    end              
+
+    desc "soft_reboot [SLICE]", "perform a soft reboot"
+    def soft_reboot(slice_name = nil)
+      abort "please provice a slice name" if slice_name.nil?
+      
+      puts "(soft) rebooting #{slice_name}"
+      reboot(:reboot, slice_name)
+    end
+
+
+    protected
+
+      def select_image_from(images)
+        images.each_index { |i| puts "[#{i}] #{images[i].name}" } 
+        print "Select an Image by ID: "
+        input = STDIN.gets.chomp.to_i
+        images[input].id
+      end
+
+      def select_flavor_from(flavors)
+        flavors.each_index { |i| puts "[#{i}] #{flavors[i].name} (#{flavors[i].ram}mb) ($#{flavors[i].price}/month)" }
+        print "Select an Image by ID: "
+        input = STDIN.gets.chomp.to_i
+        flavors[input].id
+      end
+
+      def reboot(type, slice_name)
+        slice = Resources::Slice.find_by_name(slice_name)
+        slice.put(type)
+      end
+    
+    public
+        
     # init thor
-    start
+    start                                             
   end
 end
